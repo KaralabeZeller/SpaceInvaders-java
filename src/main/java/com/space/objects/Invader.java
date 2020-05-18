c4fc1c400a3be2da3399b06e3da633228398264c
KaralabeZeller <g.fertetics@hotmail.com>
2020-05-18 12:08:35 +0200

    Random invader mesh alternate within a row

--- src/main/java/com/space/objects/Invader.java
+++ src/main/java/com/space/objects/Invader.java
@@ -4,20 +4,16 @@ import com.space.engine.graphics.Mesh;
 import com.space.engine.maths.Vector3f;
 import com.space.engine.objects.GameObject;
 import com.space.util.Constants;
+import com.space.util.Utils;
 
 public class Invader extends GameObject {
 
     public float initialX;
 
-    public Invader(Vector3f position, Vector3f rotation, Vector3f scale, Mesh mesh, Constants.ObjecType type) {
-        super(position,rotation,scale,mesh);
-        initialX = position.getX();
-        setObjecType(type);
-    }
-
     public Invader(Vector3f position, Vector3f rotation, Vector3f scale, Mesh mesh1, Mesh mesh2, Constants.ObjecType monster) {
         super(position,rotation,scale,mesh1, mesh2);
         initialX = position.getX();
         setObjecType(monster);
+        setAlternateMesh(Utils.getRandomBoolean()); // TODO do we need random aliens meshes per row?
     }
 }
---
+++ src/main/java/com/space/util/Utils.java
@@ -0,0 +1,7 @@
+package com.space.util;
+
+public class Utils {
+    public static boolean getRandomBoolean() {
+        return Math.random() < 0.5;
+    }
+}
